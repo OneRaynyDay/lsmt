755185026f9c0766e458de25f98d8efdf8cb99ca
Ray Zhang <ray.zhang@airbnb.com>
2019-09-27 17:44:34 -0700

    add a bit to cmake to allow for make test

--- test/test_lsmt.cpp
+++ test/test_lsmt.cpp
@@ -1,5 +1,5 @@
 #define CATCH_CONFIG_MAIN // Provide a main
-#include <catch2/catch.hpp>
+#include <catch.hpp>
 #include <lsmt.hpp>
 #include <string>
 
@@ -7,10 +7,13 @@ using namespace lsmt;
 
 // Construction
 TEST_CASE( "Initializing an LSM Tree with no sstables is okay", "[lsmt::lsmt]" ) {
-    lsmtree<std::string, int, 0>({}, {});   
+    lsmtree<std::string, int, 0> t({}, {});
+    REQUIRE( t.size() == 0 );
 }
 
 // Inserts
 TEST_CASE( "Adding to an LSM Tree will increase its size by 1", "[lsmt::insert]" ) {
-
+    lsmtree<std::string, int, 0> t({}, {});
+    t.insert("test", 3);
+    REQUIRE( t.size() == 1 );
 }
